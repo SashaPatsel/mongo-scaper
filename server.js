87a3d79cd0fa8114378bed42bc539196da9e9528
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-03-14 21:59:39 -0700

    file structure

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ var app = express();
 var databaseUrl = "scraper";
 var collections = ["scrapedData"];
 
+app.use(express.static("public"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 // Hook mongojs configuration to the db variable
 var db = mongojs(databaseUrl, collections);
 db.on("error", function(error) {
@@ -20,34 +27,10 @@ db.on("error", function(error) {
 });
 
 
-request("https://www.theringer.com/", function(error, response, html) {
-
-  // Load the HTML into cheerio
-  var $ = cheerio.load(html);
-
-  // Make an empty array for saving our scraped info
-  var results = [];
-
-  // Cheerio
-  
-  // h2.c-entry-box--compact__title
-  $("div.c-entry-box--compact").each(function(i, element) {
+require("./routes/html-routes.js")(app);
+// require("./routes/api-routes.js")(app);
 
 
-    var articles = $(element).find("h2.c-entry-box--compact__title").text()
-    var link = $(element).children("a").attr("href")
-    var img = $(element).find("img").attr("src")
-    // Push the image's URL (saved to the imgLink var) into the results array
-    results.push({ 
-      articles: articles,
-      link: link,
-      img: img 
-     });
-  });
-
-  // After looping through each element found, log the results to the console
-  console.log(results);
-});
 
 
 // Listen on port 3000
